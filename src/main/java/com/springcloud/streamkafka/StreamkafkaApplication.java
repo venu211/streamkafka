f0aa0766bcb82186531157bf5b8fc235d3e77beb
venu211 <venudondapati2008@gmail.com>
2019-09-20 14:53:02 -0500

    Initialize Repo

---
+++ src/main/java/com/springcloud/streamkafka/StreamkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.streamkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamkafkaApplication.class, args);
+	}
+
+}
