7edec60dd75726d3b6288a5b6aaed5edb36d2de9
venu211 <venudondapati2008@gmail.com>
2019-09-23 09:21:28 -0500

    add greetings service class

--- src/main/java/com/springcloud/streamkafka/service/GreetingsService.java
+++ src/main/java/com/springcloud/streamkafka/service/GreetingsService.java
@@ -1,4 +1,30 @@
 package com.springcloud.streamkafka.service;
 
+import com.springcloud.streamkafka.model.Greetings;
+import com.springcloud.streamkafka.stream.GreetingsStreams;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
+
+@Service
+@Slf4j
 public class GreetingsService {
+    private final GreetingsStreams greetingsStreams;
+
+    public GreetingsService(GreetingsStreams greetingsStreams) {
+        this.greetingsStreams = greetingsStreams;
+    }
+
+    public void sendGreeting(final Greetings greetings) {
+        log.info("Sending greetings {}", greetings);
+
+        MessageChannel messageChannel = greetingsStreams.outboundGreetings();
+        messageChannel.send(MessageBuilder
+                .withPayload(greetings)
+                .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+                .build());
+    }
 }
