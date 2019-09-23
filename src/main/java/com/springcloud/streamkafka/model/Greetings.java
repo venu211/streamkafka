93bc1520e1f6376ac0656d14c735fbcf7f6597e8
venu211 <venudondapati2008@gmail.com>
2019-09-23 09:20:53 -0500

    add greetings model class

--- src/main/java/com/springcloud/streamkafka/model/Greetings.java
+++ src/main/java/com/springcloud/streamkafka/model/Greetings.java
@@ -1,4 +1,12 @@
 package com.springcloud.streamkafka.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString @Builder
 public class Greetings {
+    private long timestamp;
+    private String message;
 }
